a50119f2f33d9df7c6377cf82a9aa401cc91a838
Thiyagu <thiyaguit99@gmail.com>
2020-01-01 16:24:30 +0530

    fix

--- push.js
+++ push.js
@@ -7,5 +7,9 @@ let vapidkeys=
 
 push.setVapidDetails('mail:totest@code.co.uk',vapidkeys.publicKey,vapidkeys.privateKey)
 
-let sub={};
+let sub={
+    endpoint:"https://fcm.googleapis.com/fcm/send/cVbz2JknGBs:APA91bF0IuO7QrzzGP-Ccgb_jMxFug5ZbN5Szv2KSnasaQ71uV3uHeM3zRrUJ4iGX2TmnNDpTuTrbIvZSYzgzb_vo-YpvOTzahyqLMX8b7mMluJUOV4uMZIIFX6EP9r29YEebIQSW27q","expirationTime":null,"keys":{"p256dh":"BL63HzjnRb_hlEW6jrQFm1kU32SJ-0ZLCJ-T_V5t3Q02w9Tm94G_GbHizDc2kTmdtrjmOUCR4oyS3ALKaqSppsU",
+    auth:"2egvxmiGzHgzyU63LYt8tQ"
+}
+};
 push.sendNotification(sub,'text message')
